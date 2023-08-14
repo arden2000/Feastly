3fd3e9be97abd1dfbc2ed9cc84117b244d63d181
Arden <ardenozdere@gmail.com>
2023-08-14 16:34:55 -0700

    small style change on main tagline text

--- local-foods/app/page.tsx
+++ local-foods/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-20 	">
       <div className="flex flex-col items-center gap-y-7 text-center">
-        <p className="font-sans text-6xl font-bold text-transparent bg-clip-text animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500">
+        <p className="font-sans leading-normal text-6xl font-bold text-transparent bg-clip-text animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500">
           The new way to find local foods
         </p>
         <p className="font-sans text-xl">
