68c090d493a24cd7f96eb56cb8904d3c6b28bc0b
Arden Ozdere <arden@Ardens-Air.lan>
2023-08-28 18:52:08 -0700

    food images using google custom search

---
+++ local-foods/app/api/imageSearch/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const requestData = await req.json();
+  const foodName = requestData.foodName || "";
+  const baseUrl = `https://customsearch.googleapis.com/customsearch/v1?cx=${process.env.GOOGLE_CUSTOM_SEARCH_ID}&searchType=image&key=${process.env.GOOGLE_CUSTOM_SEARCH_API_KEY}`;
+  const query = `&q=${foodName}%20food`;
+
+  const response = await fetch(`${baseUrl}${query}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  return NextResponse.json(
+    {
+      result: await response.json(),
+    },
+    { status: 200 }
+  );
+}
--- local-foods/app/components/FoodBoxComponent.tsx
+++ local-foods/app/components/FoodBoxComponent.tsx
@@ -21,16 +21,41 @@ export default function FoodBoxComponent({
   //   setStyle("border");
   // }
 
+  const [foodImage, setFoodImage] = useState("/black_image.jpg")
+
+  const getFoodImage = async () => {
+    console.log("getting food images")
+    const response = await fetch("/api/imageSearch", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ foodName: foodInfo.name }),
+    });
+
+    const data = await response.json();
+    console.log(data.result.items[0])
+    setFoodImage(data.result.items[0].link)
+
+    if (response.status !== 200) {
+      throw (
+        data.error ||
+        new Error(`Request failed with status ${response.status}`)
+      );
+    }
+  }
+
+  getFoodImage().catch(console.error)
+
   return (
     <div
       onClick={() => setSelectedFood(foodInfo.name)}
-      className={`flex flex-row gap-4 justify-start ${
-        selectedFood === foodInfo.name
+      className={`flex flex-row gap-4 justify-start ${selectedFood === foodInfo.name
           ? "border-2 border-black scale-105"
           : "border"
-      } rounded-lg p-4 hover:scale-105 hover:border-2`}
+        } rounded-lg p-4 hover:scale-105 hover:border-2`}
     >
-      <Image src="/black_image.jpg" alt="me" width="128" height="128" />
+      <img src={foodImage} alt="me" width="128" height="128" />
       <div className="flex flex-col">
         <p className="text-lg font-medium">{foodInfo.name}</p>
         <p>{foodInfo.description}</p>
