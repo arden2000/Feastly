e1e3eb1db897bc1ed82c2efa5e1c252f1e3c47a1
Arden <ardenozere@gmail.com>
2023-08-28 21:03:19 -0700

    better usage of places api, restaurant link when clicked

---
+++ local-foods/app/api/restaurantDetails/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const requestData = await req.json();
+  const placeId = requestData.placeId 
+
+  const baseUrl = `https://maps.googleapis.com/maps/api/place/details/json?key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  const place = `&place_id=${placeId}`;
+  const fields = `&fields=url%2Cphoto`;
+
+  const response = await fetch(`${baseUrl}${place}${fields}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  console.log("getting restaurant details");
+  console.log(response);
+
+  return NextResponse.json(
+    {
+      result: await response.json(),
+    },
+    { status: 200 }
+  );
+}
--- local-foods/app/components/RestaurantBoxComponent.tsx
+++ local-foods/app/components/RestaurantBoxComponent.tsx
@@ -10,20 +10,49 @@ export default function RestaurantBoxComponent({
   restaurantInfo: google.maps.places.PlaceResult;
 }) {
   const [image, setImage] = useState("/black_image.jpg");
+  const [restaurantUrl, setRestaurantUrl] = useState()
 
+  const getRestaurantDetails = async () => {
+    const response = await fetch("/api/restaurantDetails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        placeId: restaurantInfo.place_id,
+      }),
+    });
+    const data = await response.json();
+
+    console.log("restaurant details")
+    console.log(data)
+
+    setRestaurantUrl(data.result.result.url);
+
+    if (response.status !== 200) {
+      throw (
+        data.error ||
+        new Error(`Request failed with status ${response.status}`)
+      );
+    }
+  }
   useEffect(() => {
-    setImage(restaurantInfo.photos != undefined ? restaurantInfo.photos[0].getUrl() : "/black_image.jpg");
-  }, []);
+    getRestaurantDetails().catch(console.error)
+  }, [])
+
+  // useEffect(() => {
+  //   setImage(restaurantInfo.photos != undefined ? restaurantInfo.photos[0].getUrl() : "/black_image.jpg");
+  // }, []);
 
   return (
     <div className="flex flex-row gap-4 justify-start">
       <img src={image} alt="me" width="128" height="128" />
       <div className="flex flex-col">
-        <p className="text-lg font-medium">{restaurantInfo.name}</p>
+        <a href={restaurantUrl} target="_blank" className="text-lg font-medium hover:underline">{restaurantInfo.name}</a>
         <p>{restaurantInfo.vicinity}</p>
         <p className="flex flex-row items-center">
           <AiFillStar color="orange" />
-          {restaurantInfo.rating} ({restaurantInfo.user_ratings_total}) 
+          {restaurantInfo.rating} ({restaurantInfo.user_ratings_total})
         </p>
       </div>
     </div>
