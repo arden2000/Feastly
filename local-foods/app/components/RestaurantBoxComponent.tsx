44de595f774c1b5df62d6d645fb24f64ef553681
Arden Ozdere <ardenozere@gmail.com>
2023-08-28 19:32:42 -0700

    changed to normal img

--- local-foods/app/components/RestaurantBoxComponent.tsx
+++ local-foods/app/components/RestaurantBoxComponent.tsx
@@ -17,7 +17,7 @@ export default function RestaurantBoxComponent({
 
   return (
     <div className="flex flex-row gap-4 justify-start">
-      <Image src={image} alt="me" width="128" height="128" />
+      <img src={image} alt="me" width="128" height="128" />
       <div className="flex flex-col">
         <p className="text-lg font-medium">{restaurantInfo.name}</p>
         <p>{restaurantInfo.vicinity}</p>
