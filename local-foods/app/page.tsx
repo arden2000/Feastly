b8a0591028b7a47a3a8340b0d7a5a93837b17381
arden2000 <ardenozdere@gmail.com>
2023-09-05 12:12:17 -0700

    moved home search to separate component

--- local-foods/app/page.tsx
+++ local-foods/app/page.tsx
@@ -1,28 +1,9 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link"
 import { useState, useEffect } from "react";
-import { IFoodInfo, ILocationInfo } from "./interfaces/types";
+import AutoTypeSearchLinkComponent from "./components/AutoTypeSearchLinkComponent"
 
 export default function Home() {
-  const [homeLocationInput, setHomeLocationInput] = useState("");
-  const autoTypeTexts = ["Rome", "New York", "Tokyo", "Buenos Aires", "Istanbul"].map(city => city + " ".repeat(15));
-  const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const [currentCharIndex, setCurrentCharIndex] = useState(0);
-
-  function typeNextChar() {
-    if (currentCharIndex <= autoTypeTexts[currentTextIndex].length) {
-      setCurrentCharIndex(prevValue => prevValue + 1);
-    
-    } else {
-      setCurrentCharIndex(0);
-      setCurrentTextIndex((currentTextIndex + 1) % autoTypeTexts.length);
-    }
-  }
-  useEffect(() => {
-    const interval = setInterval(typeNextChar, 150);
-    return () => clearInterval(interval)
-  }, [currentCharIndex])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-20 	">
@@ -35,27 +16,7 @@ export default function Home() {
           them
         </p>
       </div>
-      <form>
-        <input
-          type="text"
-          name="location"
-          placeholder={autoTypeTexts[currentTextIndex].slice(0, currentCharIndex)}
-          value={homeLocationInput}
-          onChange={(e) => setHomeLocationInput(e.target.value)}
-          required
-          className="rounded-lg bg-white border-2 border-black p-3 w-full"
-        />
-        <Link href={{
-          pathname: '/search',
-          query: { homeLocation: homeLocationInput },
-        }}>
-          <button
-            className="rounded-lg text-white bg-black p-1.5 w-full mt-2 hover:bg-white hover:text-black border-white border-2 hover:border-black"
-          >
-            Find Foods
-          </button>
-        </Link>
-      </form>
+      <AutoTypeSearchLinkComponent/>
     </main>
   );
 }
